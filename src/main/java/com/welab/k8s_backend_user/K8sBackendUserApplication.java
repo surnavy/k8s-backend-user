56be06addc6d05ab0643368af70756195ae48a93
surnavy <113087821+surnavy@users.noreply.github.com>
2025-06-11 17:12:09 +0900

    Update K8sBackendUserApplication.java

--- src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
+++ src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
@@ -2,6 +2,7 @@ package com.welab.k8s_backend_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
