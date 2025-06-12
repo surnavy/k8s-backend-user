e40a8177332f5736ae908db063577e5bf6c64440
surnavy <113087821+surnavy@users.noreply.github.com>
2025-06-12 11:27:33 +0900

    feat. update 0.0.3

---
+++ src/main/java/com/welab/k8s_backend_user/domain/SiteUser.java
@@ -0,0 +1,38 @@
+package com.welab.k8s_backend_user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Entity
+@Table(name = "site_user")
+public class SiteUser {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column(name = "user_id", unique = true, nullable = false)
+    @Getter
+    @Setter
+    private String userId;
+
+    @Column(name = "password", nullable = false)
+    @Getter
+    @Setter
+    private String password;
+
+    @Column(name = "phone_number", nullable = false)
+    @Getter
+    @Setter
+    private String phoneNumber;
+
+    @Column(name = "deleted", nullable = false)
+    @Getter
+    @Setter
+    private Boolean deleted = false;
+}
